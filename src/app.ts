c6dcc61684104c3f18098291f408cddb6bbaef9d
Abel Borit <abelboritguitton@gmail.com>
2024-11-12 17:32:20 -0500

    [add] listen data from server

--- src/app.ts
+++ src/app.ts
@@ -13,10 +13,18 @@ wss.on("connection", function connection(ws) {
 
   ws.on("message", function message(data) {
     /* cuando el WebSocket emita un mensaje, aquí se ejecutará algo del lado del servidor y es en tiempo real, se podría guardar de repente alguna data en la base de datos o lanzar una funcionalidad o cosas por el estilo */
-    console.log("Desde el cliente:", data);
+    // console.log("Desde el cliente:", data);
+
+    /* aquí tenemos que serializar, enviándolo como string, lo que se enviará al cliente, si no colocamos JSON.stringify(.....) nos dará un error similar a -- Argument of type '{ type: string; payload: string; }' is not assignable to parameter of type 'BufferLike'.ts(2345) -- */
+    const payload = JSON.stringify({
+      type: "custom-message",
+      payload: data.toString().toUpperCase(), // aquí estamos regresándole al cliente lo que él mismo envió al servidor pero se lo enviaremos todo en mayúsculas
+    });
+
+    ws.send(payload);
   });
 
-  ws.send("Hello from server");
+  // ws.send("Hello from server");
 
   /* aquí estamos haciendo que cada 2 segundos el servidor emita un mensaje sin que el cliente (página web) lo solicite. Esta sería su principal característica de los WebSocket */
   // setInterval(() => {
