47251ad2c075f7674486ae4edb56aebd7ab0399f
Abel Borit <abelboritguitton@gmail.com>
2024-11-12 17:09:30 -0500

    [add] send messages to server

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,19 @@
 import { WebSocketServer } from "ws";
 
+/* este "wss" es nuesto WebSocket Server */
 const wss = new WebSocketServer({ port: 3000 });
 
 wss.on("connection", function connection(ws) {
   console.log("Client connected ✅");
 
+  /* este "ws" sería el WebSocket del cliente, es decir, el que está en el "index.html". Cada instancia del navegador web es una nueva conexión de websockets */
   // console.log(ws);
 
   ws.on("error", console.error);
 
   ws.on("message", function message(data) {
-    console.log("received: %s", data);
+    /* cuando el WebSocket emita un mensaje, aquí se ejecutará algo del lado del servidor y es en tiempo real, se podría guardar de repente alguna data en la base de datos o lanzar una funcionalidad o cosas por el estilo */
+    console.log("Desde el cliente:", data);
   });
 
   ws.send("Hello from server");
